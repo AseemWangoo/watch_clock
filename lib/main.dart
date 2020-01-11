08ed43c8cb76f216e0764884b612100263da4e43
aseem wangoo <aseemwangoo@gmail.com>
2020-01-11 14:06:58 +0800

    [C/Clock] Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show SystemChrome, DeviceOrientation;
-import 'package:provider/provider.dart';
 
 import 'package:watch_clock/circular/circle.dart';
 import 'package:watch_clock/models/time.dart';
+import 'package:watch_clock/shared/widgets/change_notifier.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,34 +33,61 @@ class _Home extends StatelessWidget {
   Widget build(BuildContext context) {
     //
 
-    return ChangeNotifierProvider<TimeModel>(
-      create: (_) => TimeModel(),
-      child: Scaffold(
-        body: SafeArea(
-          child: Align(
-            child: AspectRatio(
-              aspectRatio: 5 / 3,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Consumer<TimeModel>(
-                  child: CircleProgress(),
-                  builder: (context, model, child) {
-                    //
-
-                    return Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(model.currentTime),
-                        child,
-                      ],
-                    );
-                  },
-                ),
+    return Scaffold(
+      body: SafeArea(
+        child: Align(
+          child: AspectRatio(
+            aspectRatio: 5 / 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ChangeNotifierWidget<TimeModel>(
+                child: CircleProgress(),
+                model: TimeModel(),
+                builder: (context, model, child) {
+                  //
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(model.currentTime),
+                      child,
+                    ],
+                  );
+                },
               ),
             ),
           ),
         ),
       ),
     );
+
+    // return ChangeNotifierProvider<TimeModel>(
+    //   create: (_) => TimeModel(),
+    //   child: Scaffold(
+    //     body: SafeArea(
+    //       child: Align(
+    //         child: AspectRatio(
+    //           aspectRatio: 5 / 3,
+    //           child: Padding(
+    //             padding: const EdgeInsets.all(8.0),
+    //             child: Consumer<TimeModel>(
+    //               child: CircleProgress(),
+    //               builder: (context, model, child) {
+    //                 //
+
+    //                 return Column(
+    //                   mainAxisSize: MainAxisSize.min,
+    //                   children: [
+    //                     Text(model.currentTime),
+    //                     child,
+    //                   ],
+    //                 );
+    //               },
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
